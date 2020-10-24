c4c69f8f3963776953fcaff6b704bde76403ec7d
Kaio César <kaio.araujo@mobiliza.com.br>
2020-10-24 12:45:42 -0300

    🔧 update: package.json
    ok

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from "./app";
 import "./bootstrap";
 
 app.listen(process.env.NODE_PORT, () =>
-  console.log(`Salesfy app listening on port ${process.env.NODE_PORT}!`)
+  console.log(`Salesfy app listening on port ${process.env.NODE_PORT}`)
 );
